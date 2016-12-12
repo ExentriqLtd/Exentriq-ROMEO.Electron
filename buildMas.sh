48954709dfeebc13c3b8a7b671690185f9c2892f
VeselyT <tomas@vesely.in>
2016-12-12 12:01:01 +0100

    added script to create bundle for MAS release

---
+++ buildMas.sh
@@ -0,0 +1,2 @@
+#/bin/bash 
+npm run release -- --mas --mas-sign "3rd Party Mac Developer Application: Exentriq Ltd. (G2W8M47U3E)" --mas-installer-sign "3rd Party Mac Developer Installer: Exentriq Ltd. (G2W8M47U3E)"
